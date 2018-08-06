54b92446b6dc5f685bacb8dd09720de78f8650ff
Evgeniy Baranov <baranov@example.com>
2018-08-06 21:16:48 +0400

    Modify file ConnectThread.java in newBranch

--- src/main/java/com/markava/itcompany/connectionpool/ConnectThread.java
+++ src/main/java/com/markava/itcompany/connectionpool/ConnectThread.java
@@ -38,8 +38,9 @@ public class ConnectThread extends Thread {
 	public static void main(String[] args) {
 
 		LazyConnectionPool conPool = LazyConnectionPool.getInstance();
-		for (int i = 0; i < 7; i++) {
+		for (int i = 0; i < 10; i++) {
 			ConnectThread thread = new ConnectThread(conPool);
+			logger.log(Level.INFO, "Thread number "+ i+ " is created");
 			thread.start();
 		}
 	}
